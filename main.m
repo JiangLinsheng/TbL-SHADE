f0f09b4bdbb6c5bb6c937f965f9ce46b5af3e4d5
JiangLinsheng <45457209+JiangLinsheng@users.noreply.github.com>
2020-10-21 13:15:59 +0800

    Update main.m

--- main.m
+++ main.m
@@ -4,11 +4,11 @@ clear all
 %setenv('MW_MINGW64_LOC','E:\mingw-w64\x86_64-5.3.0-posix-seh-rt_v4-rev0\mingw64');
 clc
 %mex cec20_func.cpp -DWINDOWS
-D=5;%维度
-Xmin=-100;
-Xmax=100;
-pop_size=100;
-runs=30;
+D=5;%the dimension can be 5,10,15 or 20
+Xmin=-100;%lower bound
+Xmax=100;%upper bound
+pop_size=100;%population size
+runs=30;%the number of independent repeated experiments
 switch D
     case 5
         EFS=50000;
@@ -19,17 +19,17 @@ switch D
     case 20
         EFS=10000000;
     otherwise
-        a='维度不存在';
+        a='the dimension does not exist';
         fprintf('%s',a);
 end
 fhd=str2func('cec20_func');
 
-TbL_SHADE_D5=zeros(16,runs,10);
-TbL_SHADE_D5_result=zeros(10,5);%存储每次重复每个函数的最优值
+TbL_SHADE_D5=zeros(16,runs,10);%Experimental results required by CEC2020
+TbL_SHADE_D5_result=zeros(10,5);%Store the optimal value of each test function in each repeated experiment
 fbest=zeros(10,runs);
 pop_anay=zeros(runs,3,10);
-TbL_SHADE_anay_result_D5=zeros(10,3);%种群聚类和种群密度结果
-for i=1:10%函数序号
+TbL_SHADE_anay_result_D5=zeros(10,3);%the results of population clustering and population density
+for i=1:10%the number of test function
     func_num=i;
     for j=1:runs
         i,j,
@@ -44,12 +44,12 @@ for i=1:10%函数序号
 end
 for i=1:10
     temp_1=pop_anay(:,:,i);
-    a=sum(temp_1(:,1)==1);%出现聚类的实验次数
+    a=sum(temp_1(:,1)==1);%the number of runs (#runs) of population aggregation
     TbL_SHADE_anay_result_D5(i,1)=a;
     if(a>=1)
         temp_2=temp_1(temp_1(:,1)==1,:);
-        TbL_SHADE_anay_result_D5(i,2)=mean(temp_2(:,2));
-        TbL_SHADE_anay_result_D5(i,3)=mean(temp_2(:,3));
+        TbL_SHADE_anay_result_D5(i,2)=mean(temp_2(:,2));%the average generation (Mean CO) of the first cluster during these runs
+        TbL_SHADE_anay_result_D5(i,3)=mean(temp_2(:,3));%the average population diversity (Mean PD) of these generations
     end
 end
 
