b1e83e87b7e0c6e8f55cc32a4a51e735e1329bb6
JiangLinsheng <45457209+JiangLinsheng@users.noreply.github.com>
2020-10-21 13:06:46 +0800

    Update TbL_SHADE.m

--- TbL_SHADE.m
+++ TbL_SHADE.m
@@ -4,25 +4,25 @@
 function [g_best,error_value_1,pop_anay]=TbL_SHADE(fhd,D,pop_size,Xmin,Xmax,EFS,varargin)
 format long;
 rand('state',sum(100*clock));
-Np = pop_size;%种群大小
+Np = pop_size;%Population size
 Np_init=pop_size;
 Np_min=4;
-xmin = Xmin;%最小下界
-xmax = Xmax;%最大上界
-value = zeros(1,Np);%适应度值
-value_1=zeros(1,Np);%存储上一代的适应度值，因为sort排序使value改变了
-pop_next_1=zeros(Np,D);%变异交叉后的种群
-pop_next=zeros(Np,D);%选择后的下一代种群
+xmin = Xmin;%lower bound
+xmax = Xmax;%upper bound
+value = zeros(1,Np);%fitness value
+value_1=zeros(1,Np);%Store the fitness value of the previous generation, because sort() changes the value array
+pop_next_1=zeros(Np,D);%Population after mutation and crossover
+pop_next=zeros(Np,D);%Next generation population after selection
 A=zeros(Np,D);
 A_pos=0;
-g=1;%迭代次数
-efs=0;%适应度评估次数
+g=1;%the number of generation
+efs=0;%the number of the calculation of the fitness function
 min_error=1000;
-error_value_1=zeros(16,1,1);%统计实验结果
+error_value_1=zeros(16,1,1);%Statistical experiment results
 memory_pos=1;
-k=0;%统计实验结果用
-optimum=[100,1100,700,1900,1700,1600,2100,2200,2400,2500];%统计实验结果用
-%种群多样性分析用
+k=0;
+optimum=[100,1100,700,1900,1700,1600,2100,2200,2400,2500];%The optimal value of 10 test functions
+%the parameters of population diversity analysis
 pop_anay=zeros(1,3,1);
 epsilon=2;
 MinPts=4;
@@ -33,7 +33,7 @@ function y = f(pos)
         error_y=0;
         min_error=0;
     end
-    efs=efs+1;%适应度评估次数加一
+    efs=efs+1;%the number of the calculation of the fitness function self adding
     if efs<=EFS
         spefs=round(power(D,(k/5-3))*EFS);
         if efs == spefs
@@ -43,9 +43,9 @@ function y = f(pos)
     end
 end
 
-%反向进化SHADE参数
-distance_init=0;%初始反向距离
-distance_min=0;%最小反向距离
+%the parameters of turning-based mutation
+distance_init=0;%the initial outer radius
+distance_min=0;%the inner radius
 for di=1:D
     distance_init=distance_init+power(100,2);
     distance_min=distance_min+power(5,2);
@@ -53,15 +53,15 @@ end
 distance_init=sqrt(distance_init);
 distance_min=sqrt(distance_min);
 
-pop=rand(Np,D)*(xmax-xmin)+xmin;%初始化种群
-for m=1:Np%求适应度值
+pop=rand(Np,D)*(xmax-xmin)+xmin;% initialize the population
+for m=1:Np%Calculate the fitness value
     value(m)=f(pop(m,:));
 end
-g_best=min(value);%全局最优值
-picture_data(g)=g_best;
+g_best=min(value);%Global optimal value
+picture_data(g)=g_best;%Data for drawing
 value_1=value;
-Mf=zeros(1,Np);%缩放因子（变异因子)历史记忆
-Mcr=zeros(1,Np);% 交叉因子历史记忆
+Mf=zeros(1,Np);%the historical memory of scaling factor F
+Mcr=zeros(1,Np);%the historical memory of crossover rate CR
 Mf=Mf+0.5;
 Mcr=Mcr+0.5;
 while (efs<EFS && min_error>=power(10,-8))
@@ -74,25 +74,25 @@ while (efs<EFS && min_error>=power(10,-8))
         end
     end
     
-    Sf=zeros(1,Np);%暂存每次迭代中较好的缩放因子
-    Scr=zeros(1,Np);%暂存每次迭代中较好的交叉因子
-    dif_fitness=zeros(1,Np);%适应度差值
-    dif_fitness_sum=0;%适应度差值之和
-    dif_sum=0;%差值个数
-    [~,b]=sort(value,2);%排序
+    Sf=zeros(1,Np);%Hold the better scaling factor F in each generation
+    Scr=zeros(1,Np);%Hold the better crossover rate CR in each generation
+    dif_fitness=zeros(1,Np);%the difference of fitness
+    dif_fitness_sum=0;%the sum of the difference of fitness
+    dif_sum=0;%the number of difference
+    [~,b]=sort(value,2);%sort
     distance=round(((distance_min-distance_init)/EFS)*efs+distance_init);
     for i=1:Np
         
-        %%%%%%%%%%%%%%%%%%%%%%%%----变异操作----%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%----mutation----%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         cri=randi(Np);
         fi=cri;
-        CRi=normrnd(Mcr(1,cri), 0.1);%正态分布
+        CRi=normrnd(Mcr(1,cri), 0.1);%Normal distribution
         if(CRi>=1)
             CRi=1;
         elseif(CRi<=0)
             CRi=0;
         end
-        Fi= Mf(1,fi) + 0.1 * tan(pi * (rand - 0.5));%柯西分布
+        Fi= Mf(1,fi) + 0.1 * tan(pi * (rand - 0.5));%Cauchy distribution
         while(Fi<=0)
             Fi= Mf(1,fi) + 0.1 * tan(pi * (rand - 0.5));
         end
@@ -101,9 +101,9 @@ while (efs<EFS && min_error>=power(10,-8))
         end
         
         ps=rand*(0.2-2/Np)+2/Np;
-        Pbest=b(randi(round(Np*ps)));%取较好解的前%ps中的一个解
+        Pbest=b(randi(round(Np*ps)));%the individual selected from the best NP × ps (ps ∈ [0,1]) individuals randomly in the current population 
          dx = randperm(Np);          
-         r1=dx(1);%另外两个向量
+         r1=dx(1);%The other two individuals
          if r1 == i
             r1= dx(2);
          end
@@ -126,19 +126,20 @@ while (efs<EFS && min_error>=power(10,-8))
              par_distance=par_distance+power((pop(i,dj)-pop(Pbest,dj)),2);
          end
          par_distance=sqrt(par_distance);
-         temp_de=Fi*(pop(Pbest,:)-pop(i,:))+Fi*(pop(r1,:)-pop_1(r2g,:));%差分向量
+         temp_de=Fi*(pop(Pbest,:)-pop(i,:))+Fi*(pop(r1,:)-pop_1(r2g,:));%Difference vector
          if par_distance<distance && par_distance>distance_min
              temp_de=-temp_de;
-             temp_d=randi(D);%随机选取的维度数
-             rand_d=randperm(D);%随机选取的维度
+             temp_d=randi(D);%the number of randomly selected dimensions
+             rand_d=randperm(D);%the randomly disordered dimension index array
              for d=1:temp_d
-                 temp_de(rand_d(d))=rand*(xmax-xmin)+xmin;%折射操作
+                 temp_de(rand_d(d))=rand*(xmax-xmin)+xmin;
              end
          end
-         son=pop(i,:)+temp_de;%变异
+         son=pop(i,:)+temp_de;%turning based mutation
          
+          %%%%%%%%%%%%%%%%%%----boundary-handling and crossover---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
          jrand=randi(D);
-         for j = 1: D%边界处理、交叉操作
+         for j = 1: D
             while(son(1,j)<xmin || son(1,j)>xmax)
                  if(son(1,j)<xmin)
                      son(1,j) =(pop(i,j)+xmin)/2;
@@ -153,7 +154,7 @@ while (efs<EFS && min_error>=power(10,-8))
                     pop_next_1(i,j)=pop(i,j);
                  end
          end
-          %%%%%%%%%%%%%%%%%%----选择操作---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          %%%%%%%%%%%%%%%%%%----selection---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
          old_fit=value_1(i);
          new_fit=f(pop_next_1(i,:));
          if old_fit<=new_fit
@@ -169,9 +170,9 @@ while (efs<EFS && min_error>=power(10,-8))
             else
                 A(randi(Np),:)=pop(i,:);
             end
-            Scr(1,dif_sum)=CRi;%较好的交叉因子
-            Sf(1,dif_sum)=Fi;%较好的变异因子
-            dif_fitness(1,dif_sum)=old_fit-new_fit;%适应度差值
+            Scr(1,dif_sum)=CRi;%the better crossover rate CR
+            Sf(1,dif_sum)=Fi;%the better scaling factor F
+            dif_fitness(1,dif_sum)=old_fit-new_fit;%the difference of fitness
          end
          value_1(i) = value(i);
     end
@@ -179,7 +180,7 @@ while (efs<EFS && min_error>=power(10,-8))
     if temp_g_best<g_best
         g_best=temp_g_best;
     end
-    %更新缩放因子和交叉率历史记忆
+    %update the historical memories of scaling factor F and crossover rate CR
     if(dif_sum>0)
         dif_fitness_sum=sum(dif_fitness);
         Mcr(1,memory_pos)=0;
@@ -198,10 +199,11 @@ while (efs<EFS && min_error>=power(10,-8))
         end
     end
     pop=pop_next;
+    %Linear Decrease in Population Size
     Np_next=round((Np_min-Np_init)/EFS*efs+Np_init);
     if(Np_next<Np)
         while(Np>Np_next)
-            [~,max_index]=max(value);%排序
+            [~,max_index]=max(value);%sort
             pop(max_index,:)=[];
             A(randi(Np),:)=[];
             value(:,max_index)=[];
